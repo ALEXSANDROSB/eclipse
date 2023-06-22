d8df67714e44f6534c4adc76ad2514c24a500f8f
AlexsandroSB <120571668+ALEXSANDROSB@users.noreply.github.com>
2023-06-22 00:42:21 -0300

    adicionei arquivos via upload

---
+++ OLA GIT HUB/src/br/com/alexsandrsb/git/OLAGIT.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.alexsandrsb.git;
+
+/**
+ * 
+ * Exemplo de integração do eclipse com o GITHub
+ */
+public class OLAGIT {
+
+	
+	public static void main(String[] args) {
+      System.out.println("OLÁ GIT È NOIS");
+
+	}
+
+}
